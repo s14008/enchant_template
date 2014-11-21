fe06b867ed5b76a0df568f6a6fdd0ac5daf30d06
yamauchi@std.it-college.ac.jp <s14008@std.it-college.ac.jp>
2014-11-21 20:19:43 +0900

    enchant

---
+++ main.js
@@ -0,0 +1,21 @@
+enchant();
+
+var game;
+
+var Timer = Class.create(Label,{
+  initialize:function () {
+  this.
+
+
+  }
+});
+
+
+window.onload = function() {
+ game = new Core();
+
+game.onload = function() {}
+    game.start();
+
+
+};
